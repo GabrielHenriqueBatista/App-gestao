3595240ae77ea6ae42ec8adcf589dc925617a41a
Gabriel Henrique <gabrielhenriquebatista31@gmail.com>
2021-08-22 18:34:39 -0300

    Otimizado template tela Welcome.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="{{ asset('img/logo.png') }}" height="50" width="180"/>
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
@@ -48,11 +48,11 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Já possui cadastro?') }}
                 </a>
 
                 <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Registrar') }}
                 </x-jet-button>
             </div>
         </form>
